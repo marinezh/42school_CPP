46c51e83901bfa7c2f0370f2f89bb59091c72389
marinezh <mazhivotova@gmail.com>
2025-09-09 00:52:29 +0300

    add ex04/module01

---
+++ module_01/ex04/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+	if (argc != 4){
+		std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
+		return 1;
+	}
+	std::string file_name = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	if ( s1.empty() || file_name.empty()) {
+		std::cerr << "Error: Invalid input" << std::endl;
+		return 1;
+	}
+	std::ifstream input(file_name);
+	if (!input.is_open()){
+		std::cerr << "Error: cannot open input file" << std::endl;
+		return 1;
+	}
+	std::ofstream output(file_name + ".replace");
+	if (!output.is_open()) {
+		std::cerr << "Error: Could not create output file" << std::endl;
+		return 1;
+	}
+	std::string line;
+	while(std::getline(input, line))
+	{
+		size_t pos = 0;
+		while ((pos = line.find(s1, pos)) != std::string::npos) {
+ 			 line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+			pos += s2.length();
+		}
+		output << line << std::endl;
+	}
+	return 0;
+}
