e84f1e1b0bace7e549916b59e324608ff50fd897
Marina Zhivotova <mazhivotova@gmail.com>
2025-09-11 19:34:21 +0300

    code optimization

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -52,14 +52,12 @@ int main(int argc, char **argv)
 	std::string file_name = argv[1];
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
-	std::cout << "??????"<< file_name << " " << s1 << " "<<  s2 << std::endl;
 	if (s1 == "\\n")
 		s1 = "\n";
 	if (s2 == "\\n")
 		s2 = "\n";
 	if (s1 == "\n" && s2 != "\n")
 		s2 += "\n";
-	std::cout << "!!!!!!!"<< file_name << " " << s1 << " "<<  s2 << std::endl;
 	if (s1.empty() || file_name.empty()) {
 		std::cerr << "Error: Invalid input" << std::endl;
 		return 1;
@@ -70,11 +68,12 @@ int main(int argc, char **argv)
 		std::cerr << "Error: cannot open input file" << std::endl;
 		return 1;
 	}
+	
 	// read the entire file into one string
 	std::ostringstream buffer;
 	buffer << input.rdbuf();
 	std::string content = buffer.str();
-	
+	std::cout << "Content " << content <<  std::endl;
 	std::ofstream output(file_name + ".replace");
 	if (!output.is_open()) {
 		std::cerr << "Error: Could not create output file" << std::endl;
@@ -82,8 +81,16 @@ int main(int argc, char **argv)
 	}
 	// replace all occurrences of s1 with s2
 	size_t pos = 0;
+	//std::cout << "pos is" << pos <<  std::endl;
 	while ((pos = content.find(s1, pos)) != std::string::npos) {
+		std::cout << "!!!pos is" << pos << s1 <<  std::endl;
 		content = content.substr(0, pos) + s2 + content.substr(pos + s1.length());
+		std::cout << "Content 2 " << content <<  std::endl;
+
+		std::string sub = content.substr(0, pos);
+		std::cout << "substr " << sub <<  std::endl;
+
+		
 		pos += s2.length();
 	}
 
