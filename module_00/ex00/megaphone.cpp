e1fd9cbfa803e836fc94435c78998df963554ae3
Marina Zhivotova <mazhivotova@gmail.com>
2025-09-17 16:16:11 +0300

    make megaphone task to be done in CPP style

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include <cctype>
 
+class Megaphone {
+
+	public:
+
+		void toUpper(int ac, char **av)
+		{
+			if (ac == 1)
+			{
+				std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+				return;
+			}
+			for (int i = 1; i < ac; i++)
+			{
+				std::string string(av[i]); // converts the C-string (char*) into a C++ string object (std::string).
+				for (char& c : string) {
+					unsigned char uc = static_cast<unsigned char>(c);
+					c = static_cast<char>(std::toupper(uc));
+				}
+				std::cout << string;
+			}
+			std::cout << std::endl;
+		}
+};
+
 int main( int ac, char **av){
 
-	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return 0;
-	}
-	for (int i = 1; i < ac; i++)
-	{
-		for (int j = 0; av[i][j] != '\0'; ++j){
-			unsigned char c = av[i][j];
-			std::cout << static_cast<char>(std::toupper(c));
-		}
-	}
-	std::cout << std::endl;
+	Megaphone megaphone;
+
+	megaphone.toUpper(ac, av);
 	return 0;
 }
