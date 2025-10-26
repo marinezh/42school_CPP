2961e16e59cfba0f2cbc9a236e5432629e7947f9
marinezh <mazhivotova@gmail.com>
2025-10-26 23:10:07 +0200

    fix minore issues

--- module_03/ex02/ScavTrap.hpp
+++ module_03/ex02/ScavTrap.hpp
@@ -4,12 +4,6 @@
 #include "ClapTrap.hpp"
 
 class ScavTrap : public ClapTrap {
-	private:
-		std::string _name;
-		int 		_hitPoints;
-		int			_energyPoints;
-		int			_attackDamage;
-
 	public:
 		ScavTrap();
 		ScavTrap(std::string name);
