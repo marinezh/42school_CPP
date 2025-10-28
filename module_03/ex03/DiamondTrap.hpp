1a66ffc40acf0a661dbb2b99f7dff5ab4fe26a18
Marina Zhivotova <mazhivotova@gmail.com>
2025-10-28 15:53:22 +0200

    add debug fnction

--- module_03/ex03/DiamondTrap.hpp
+++ module_03/ex03/DiamondTrap.hpp
@@ -19,6 +19,15 @@ public:
 
 	void whoAmI();
 	using ScavTrap::attack;  // use ScavTrap's attack()
+	void printData() {
+		std::cout << "----------------------------------------" << std::endl;
+		std::cout << "DiamondTrap name: " << _name << std::endl;
+		std::cout << "ClapTrap name:    " << ClapTrap::_name << std::endl;
+		std::cout << "Hit Points:       " << _hitPoints << std::endl;
+		std::cout << "Energy Points:    " << _energyPoints << std::endl;
+		std::cout << "Attack Damage:    " << _attackDamage << std::endl;
+		std::cout << "----------------------------------------" << std::endl;
+	}
 };
 
 #endif
--- module_03/ex03/main.cpp
+++ module_03/ex03/main.cpp
@@ -4,54 +4,57 @@
 #include "DiamondTrap.hpp"
 
 int main() {
-	std::cout << "--- ClapTrap ---" << std::endl;
+	// std::cout << "--- ClapTrap ---" << std::endl;
 	ClapTrap c("BeeBot");
-	c.attack("enemy");
-	c.takeDamage(3);
-	c.beRepaired(2);
+	// c.attack("enemy");
+	// c.takeDamage(3);
+	// c.beRepaired(2);
 
-	std::cout << "\n--- ScavTrap ---" << std::endl;
+	// std::cout << "\n--- ScavTrap ---" << std::endl;
 	ScavTrap s("Guardian");
-	s.attack("enemy");
-	s.guardGate();
+	// s.attack("enemy");
+	// s.guardGate();
 
-	std::cout << "\n--- FragTrap ---" << std::endl;
+	// std::cout << "\n--- FragTrap ---" << std::endl;
 	FragTrap f("Boomer");
-	f.attack("enemy");
-	f.highFivesGuys();
+	// f.attack("enemy");
+	// f.highFivesGuys();
 
 	std::cout << "\n--- DiamondTrap ---" << std::endl;
 	DiamondTrap d("Diamond");
+	d.printData();
 	d.whoAmI();
 	d.attack("enemy");
 	d.guardGate();
 	d.highFivesGuys();
+	d.printData();
 
-	std::cout << "=== DiamondTrap Extended Tests ===" << std::endl;
+
+	// std::cout << "=== DiamondTrap Extended Tests ===" << std::endl;
 	
-	std::cout << "\n--- Test 1: Default constructor ---" << std::endl;
-	DiamondTrap d1;
-	d1.whoAmI();
+	// std::cout << "\n--- Test 1: Default constructor ---" << std::endl;
+	// DiamondTrap d1;
+	// d1.whoAmI();
 	
-	std::cout << "\n--- Test 2: Parameterized constructor ---" << std::endl;
-	DiamondTrap d2("TestBot");
-	d2.whoAmI();
+	// std::cout << "\n--- Test 2: Parameterized constructor ---" << std::endl;
+	// DiamondTrap d2("TestBot");
+	// d2.whoAmI();
 	
-	std::cout << "\n--- Test 3: Copy constructor ---" << std::endl;
-	DiamondTrap d3(d2);
-	d3.whoAmI();
+	// std::cout << "\n--- Test 3: Copy constructor ---" << std::endl;
+	// DiamondTrap d3(d2);
+	// d3.whoAmI();
 	
-	std::cout << "\n--- Test 4: Assignment operator ---" << std::endl;
-	DiamondTrap d4;
-	d4 = d2;
-	d4.whoAmI();
+	// std::cout << "\n--- Test 4: Assignment operator ---" << std::endl;
+	// DiamondTrap d4;
+	// d4 = d2;
+	// d4.whoAmI();
 	
-	std::cout << "\n--- Test 5: All abilities ---" << std::endl;
-	d2.attack("target");
-	d2.takeDamage(50);
-	d2.beRepaired(25);
-	d2.guardGate();
-	d2.highFivesGuys();
+	// std::cout << "\n--- Test 5: All abilities ---" << std::endl;
+	// d2.attack("target");
+	// d2.takeDamage(50);
+	// d2.beRepaired(25);
+	// d2.guardGate();
+	// d2.highFivesGuys();
 
 	std::cout << "\n--- End of Program ---" << std::endl;
 	return 0;
